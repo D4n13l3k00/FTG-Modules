60bb5ab0f8bbf4488be932eb05b44b519483d934
D4n13l3k00 <D4n13l3k00@yandex.ru>
2023-05-07 18:17:20 +0300

    return modules

---
+++ Try.py
@@ -0,0 +1,22 @@
+#   Coded by D4n13l3k00    #
+#     t.me/D4n13l3k00      #
+# This code under AGPL-3.0 #
+
+import random
+
+from .. import loader, utils
+
+
+@loader.tds
+class TryMod(loader.Module):
+    """Try"""
+    strings = {'name': 'Try'}
+
+    @loader.owner
+    async def trycmd(self, message):
+        """.try действие
+        """
+        do = utils.get_args_raw(message)
+        rnd = random.choice(["Удачно", "Удачно", "Неудачно", "Неудачно",
+                            "Удачно", "Неудачно", "Удачно", "Неудачно", "Удачно", "Неудачно"])
+        await message.edit(f"<b>{do}</b>\n\n<code>{rnd}</code>")
